f028aac24373d05166747ef6d9e59bb037fe3224
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2026-02-04 23:41:56 +0530

    feature: support for android edge to edge (#8)

--- index.d.ts
+++ index.d.ts
@@ -198,20 +198,20 @@ declare module "react-native-image-crop-picker" {
         cropperActiveWidgetColor?: string;
 
         /**
-         * When cropping image, determines the color of StatusBar.
+         * When cropping image, true for light status bar (dark icons), false for dark status bar (light icons).
          *
          * @platform Android only
-         * @default '#424242'
+         * @default true
          */
-        cropperStatusBarColor?: string;
+        cropperStatusBarLight?: boolean;
 
         /**
-         * When cropping image, determines the color of Toolbar.
+         * When cropping image, true for light navigation bar (dark icons), false for dark navigation bar (light icons).
          *
          * @platform Android only
-         * @default '#424242'
+         * @default false
          */
-        cropperToolbarColor?: string;
+        cropperNavigationBarLight?: boolean;
 
         /**
          * When cropping image, determines the color of Toolbar text and buttons.
